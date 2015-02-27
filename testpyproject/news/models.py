73f2cc2d99525d6ddfed42ba0b3ef4dc0808cfaf
Gennady Andreyev <gennady.andreyev@gmail.com>
2015-02-27 21:40:01 +0300

    test project

--- testpyproject/news/models.py
+++ testpyproject/news/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
-# Create your models here.
 
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    datetime = models.DateTimeField(u'Date of Publication')
+    content = models.TextField(max_length=10000)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/news/%i/" % self.id
